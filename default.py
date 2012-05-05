2f05fd8380065fa5399f24ecf50659fdf322ee0f
Geert Smelt <gasmelt@gmail.com>
2012-05-05 20:00:14 +0200

    fixed cleaning pause during library update

--- default.py
+++ default.py
@@ -13,15 +13,15 @@ import xbmcaddon
 import sqlite3
 
 # Addon info
-__title__ = 'XBMC File Cleaner'
-__author__ = 'Andrew Higginson <azhigginson@gmail.com>'
+__title__ = "XBMC File Cleaner"
+__author__ = "Andrew Higginson <azhigginson@gmail.com>"
 __addonID__ = "script.filecleaner"
-__icon__ = 'special://home/addons/' + __addonID__ + '/icon.png'
+__icon__ = "special://home/addons/" + __addonID__ + "/icon.png"
 __settings__ = xbmcaddon.Addon(__addonID__)
 
 # Autoexec info
-AUTOEXEC_PATH = xbmc.translatePath('special://home/userdata/autoexec.py')
-AUTOEXEC_SCRIPT = 'import time;time.sleep(5);xbmc.executebuiltin("XBMC.RunScript(special://home/addons/script.filecleaner/default.py,-startup)")'
+AUTOEXEC_PATH = xbmc.translatePath("special://home/userdata/autoexec.py")
+AUTOEXEC_SCRIPT = "import time;time.sleep(5);xbmc.executebuiltin('XBMC.RunScript(special://home/addons/script.filecleaner/default.py,-startup)')"
 
 class Main:
     
@@ -32,7 +32,7 @@ class Main:
         # TODO: Modify the abortRequested so that xbmcfc doesn't sleep when an abort request comes in. 
         # Put this check at the start of the addon, instead of after a possible sleep
         reload(sys)
-        sys.setdefaultencoding('utf-8')
+        sys.setdefaultencoding("utf-8")
         self.reload_settings()
         
         if self.removeFromAutoExec:
@@ -70,7 +70,7 @@ class Main:
             cleaningRequired = False
             
             if self.deleteMovies:
-                movies = self.get_expired('movie')
+                movies = self.get_expired("movie")
                 if movies:
                     for file, path in movies:
                         if os.path.exists(path):
@@ -83,15 +83,15 @@ class Main:
                             self.delete_file(path)
             
             if self.deleteTVShows:
-                episodes = self.get_expired('episode')
+                episodes = self.get_expired("episode")
                 if episodes:
                     for file, path, show, season, idFile in episodes:
                         if os.path.exists(path):
                             cleaningRequired = True
                         if self.enableHolding:
-                            if self.createSeriesSeasonDirs:
+                            if self.createSubdirectories:
                                 newpath = os.path.join(self.holdingFolder, show, "Season " + season)
-                                self.create_season_dirs(newpath)
+                                self.create_subdirectories(newpath)
                             else:
                                 newpath = self.holdingFolder
                             self.debug("Moving episode %s from %s to %s" % (os.path.basename(file), os.path.dirname(file), newpath))
@@ -106,14 +106,16 @@ class Main:
                 # Wait 10 seconds for deletions to finish before cleaning.
                 time.sleep(10)
                 
+                pause = 5
+                iterations = 0
+                limit = self.scanInterval - pause
                 # Check if the library is being updated before cleaning up
-                while (xbmc.getCondVisibility('Library.IsScanningVideo')  == True):
-                    pause = 5
-                    iterations = 0
-                    limit = self.scanInterval - pause
+                while (xbmc.getCondVisibility("Library.IsScanningVideo")):
+                    iterations += 1
                     
                     # Make sure we don't mess up the scan interval timing by waiting too long.
                     if (iterations * pause >= limit):
+                        iterations = 0
                         break
                         
                     self.debug("The video library is currently being updated, waiting %d minutes before cleaning up." % pause)
@@ -167,10 +169,10 @@ class Main:
         
         try:
             # After building the query we can use it on all video databases
-            folder = os.listdir(xbmc.translatePath('special://database/'))
+            folder = os.listdir(xbmc.translatePath("special://database/"))
             for database in folder:
-                if database.startswith('MyVideos') and database.endswith('.db'):
-                    con = sqlite3.connect(xbmc.translatePath('special://database/' + database))
+                if database.startswith("MyVideos") and database.endswith(".db"):
+                    con = sqlite3.connect(xbmc.translatePath("special://database/" + database))
                     cur = con.cursor()
                     
                     self.debug("Executing query on %s: %s" % (database, query))
@@ -208,28 +210,28 @@ class Main:
                     cur = con.cursor()
                     
                     # Insert path if it doesn't exist
-                    query = 'INSERT OR IGNORE INTO'
-                    query += ' path(strPath)'
-                    query += ' values("%s/")' % (newPath)
+                    query = "INSERT OR IGNORE INTO"
+                    query += " path(strPath)"
+                    query += " values('%s/')" % (newPath)
                     
-                    self.debug('Executing query on %s: %s' % (database, query))
+                    self.debug("Executing query on %s: %s" % (database, query))
                     cur.execute(query)
                     
                     # Look up the id of the new path
-                    query = 'SELECT idPath'
-                    query += ' FROM path'
-                    query += ' WHERE strPath = ("%s/")' % newPath
+                    query = "SELECT idPath"
+                    query += " FROM path"
+                    query += " WHERE strPath = ('%s/')" % newPath
                     
-                    self.debug('Executing ' + str(query))
+                    self.debug("Executing " + str(query))
                     cur.execute(query)
                     idPath = cur.fetchone()[0]
                     
                     # Update path reference for the moved file
-                    query = 'UPDATE OR IGNORE files'
-                    query += ' SET idPath = %d' % idPath
-                    query += ' WHERE idFile = %d' % idFile
+                    query = "UPDATE OR IGNORE files"
+                    query += " SET idPath = %d" % idPath
+                    query += " WHERE idFile = %d" % idFile
                     
-                    self.debug('Executing query on %s: %s' % (database, query))
+                    self.debug("Executing query on %s: %s" % (database, query))
                     cur.execute(query)
                     con.commit()
         except OSError, e:
@@ -250,35 +252,38 @@ class Main:
         """
         __settings__ = xbmcaddon.Addon(__addonID__)
         
-        self.serviceEnabled = bool(__settings__.getSetting('service_enabled') == "true")
-        self.delayedStart = float(__settings__.getSetting('delayed_start'))
-        self.scanInterval = float(__settings__.getSetting('scan_interval'))
-        self.showNotifications = bool(__settings__.getSetting('show_notifications') == "true")
-        self.enableExpire = bool(__settings__.getSetting('enable_expire') == "true")
-        self.expireAfter = float(__settings__.getSetting('expire_after'))
-        self.deleteOnDiskLow = bool(__settings__.getSetting('delete_on_low_disk') == "true")
-        self.lowDiskPercentage = float(__settings__.getSetting('low_disk_percentage'))
-        self.lowDiskPath = xbmc.translatePath(__settings__.getSetting('low_disk_path'))
-        self.cleanLibrary = bool(__settings__.getSetting('clean_library') == "true")
-        self.deleteMovies = bool(__settings__.getSetting('delete_movies') == "true")
-        self.deleteTVShows = bool(__settings__.getSetting('delete_tvshows') == "true")
-        self.deleteLowRating = bool(__settings__.getSetting('delete_low_rating') == "true")
-        self.lowRatingFigure = float(__settings__.getSetting('low_rating_figure'))
-        self.ignoreNoRating = bool(__settings__.getSetting('ignore_no_rating') == "true")
-        self.enableHolding = bool(__settings__.getSetting('enable_holding') == "true")
-        self.holdingFolder = xbmc.translatePath(__settings__.getSetting('holding_folder'))
-        self.enableDebug = bool(xbmc.translatePath(__settings__.getSetting('enable_debug')) == "true")
-        self.createSeriesSeasonDirs = bool(xbmc.translatePath(__settings__.getSetting('create_series_season_dirs')) == "true")
-        self.doupdatePathReference = bool(xbmc.translatePath(__settings__.getSetting('update_path_reference')) == "true")
-        self.removeFromAutoExec = bool(xbmc.translatePath(__settings__.getSetting('remove_from_autoexec')) != "false") # true
+        self.serviceEnabled = bool(__settings__.getSetting("service_enabled") == "true")
+        self.delayedStart = float(__settings__.getSetting("delayed_start"))
+        self.scanInterval = float(__settings__.getSetting("scan_interval"))
+        self.showNotifications = bool(__settings__.getSetting("show_notifications") == "true")
+        self.enableExpire = bool(__settings__.getSetting("enable_expire") == "true")
+        self.expireAfter = float(__settings__.getSetting("expire_after"))
+        self.deleteOnDiskLow = bool(__settings__.getSetting("delete_on_low_disk") == "true")
+        self.lowDiskPercentage = float(__settings__.getSetting("low_disk_percentage"))
+        self.lowDiskPath = xbmc.translatePath(__settings__.getSetting("low_disk_path"))
+        self.cleanLibrary = bool(__settings__.getSetting("clean_library") == "true")
+        self.deleteMovies = bool(__settings__.getSetting("delete_movies") == "true")
+        self.deleteTVShows = bool(__settings__.getSetting("delete_tvshows") == "true")
+        self.deleteLowRating = bool(__settings__.getSetting("delete_low_rating") == "true")
+        self.lowRatingFigure = float(__settings__.getSetting("low_rating_figure"))
+        self.ignoreNoRating = bool(__settings__.getSetting("ignore_no_rating") == "true")
+        self.enableHolding = bool(__settings__.getSetting("enable_holding") == "true")
+        self.holdingFolder = xbmc.translatePath(__settings__.getSetting("holding_folder"))
+        self.enableDebug = bool(xbmc.translatePath(__settings__.getSetting("enable_debug")) == "true")
+        self.createSubdirectories = bool(xbmc.translatePath(__settings__.getSetting("create_series_season_dirs")) == "true")
+        self.doupdatePathReference = bool(xbmc.translatePath(__settings__.getSetting("update_path_reference")) == "true")
+        self.removeFromAutoExec = bool(xbmc.translatePath(__settings__.getSetting("remove_from_autoexec")) != "false") # true
     
     def get_free_disk_space(self, path):
         """
         Determine the percentage of free disk space.
         
         Keyword arguments:
-        path -- the path to the drive to check (this can be any path of any length on the desired drive)
+        path -- the path to the drive to check (this can be any path of any length on the desired drive). 
+        If the path doesn't exist, this function returns 100, in order to prevent files from being deleted accidentally.
         """
+        percentage = 100
+        
         if os.path.exists(path):
             # First eliminate any symbolic links
             # Then remove any redundant separators and up-level references
@@ -286,10 +291,7 @@ class Main:
             drive = os.path.normpath(os.path.realpath(path))
             self.debug("The path now is " + drive)
             
-            # TODO: return 100% or 0%?
-            percentage = 0
-            
-            if platform.system() == 'Windows':
+            if platform.system() == "Windows":
                 self.debug("We are running disk space checks on a Windows file system")
                 totalNumberOfBytes = ctypes.c_ulonglong(0)
                 totalNumberOfFreeBytes = ctypes.c_ulonglong(0)
@@ -305,15 +307,6 @@ class Main:
                     percentage = float(free / capacity * float(100))
                 except ZeroDivisionError, e:
                     self.notify("Hard disk capacity is 0. Did you select the correct hard disk to check for free space?", 15000)
-                
-                self.debug("Disk space data for %s:\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s %s" % 
-                    (drive, "Total disk space (in bytes)", capacity, 
-                            "Free disk space (in bytes)", free, 
-                            "Percentage of free space", percentage,
-                            "Minimum free percentage", self.lowDiskPercentage,
-                            "Is the disk space below threshold?", percentage <= self.lowDiskPercentage
-                    )
-                )
             else:
                 self.debug("We are running checks on a non-Windows file system")
                 try:
@@ -323,12 +316,10 @@ class Main:
                     self.notify("Couldn't access %s" % self.lowDiskPath) 
                 except ZeroDivisionError, e:
                     self.notify("Hard disk capacity is 0. Did you select the correct hard disk to check for free space?", 15000)
-            
-            return percentage
         else:
             self.notify("No path selected to check for disk space. Check your settings.", 15000)
-            # TODO: return 100% or 0%?
-            return 0
+        
+        return percentage
     
     def disk_space_low(self):
         """
@@ -346,13 +337,13 @@ class Main:
                 os.remove(file)
                 self.notify(__settings__.getLocalizedString(34006) % (os.path.basename(file), os.path.dirname(file)), 10000)
             except OSError, e:
-                self.debug('Deleting file %s failed with error code %d' % (file, e.errno))
+                self.debug("Deleting file %s failed with error code %d" % (file, e.errno))
         else:
-            self.debug('The file "%s" was already deleted' % file)
+            self.debug("The file '%s' was already deleted" % file)
     
     def move_file(self, file, destination):
         """
-        Move a file to a new destination.
+        Move a file to a new destination. Returns True if the move succeeded, False otherwise.
         
         Keyword arguments:
         file -- the file to be moved
@@ -374,7 +365,7 @@ class Main:
             self.debug("Moving file %s failed with error code %d" % (file, e.errno))
             return False
     
-    def create_season_dirs(self, seasondir):
+    def create_subdirectories(self, seasondir):
         """
         Create season as well as series directories in the folder specified.
         
@@ -386,54 +377,54 @@ class Main:
         self.create_directory(seasondir)
     
     def create_directory(self, location):
-        '''
+        """
         Creates a directory at the location provided.
-        '''
+        """
         try:
-            self.debug('Creating directory at %s' % location)
+            self.debug("Creating directory at %s" % location)
             os.mkdir(location)
         except OSError, e:
             # Ignore existing directory errors
             if e.errno != errno.EEXIST:
-                self.debug('Creating directory at %s failed with error code %d' % (location, e.errno))
+                self.debug("Creating directory at %s failed with error code %d" % (location, e.errno))
                 raise
             else:
-                self.debug('Directory already exists')
+                self.debug("Directory already exists")
         else:
-            self.debug('Successfully created directory')
+            self.debug("Successfully created directory")
     
     def notify(self, message, duration=5000, image=__icon__):
-        '''
+        """
         Display an XBMC notification and log the message.
         
         Keyword arguments:
         message -- the message to be displayed and logged
         duration -- the duration the notification is displayed in milliseconds (default 5000)
         image -- the path to the image to be displayed on the notification (default "icon.png")
-        '''
+        """
         self.debug(message)
         if self.showNotifications:
-            xbmc.executebuiltin('XBMC.Notification(%s, %s, %s, %s)' % (__title__, message, duration, image))
+            xbmc.executebuiltin("XBMC.Notification(%s, %s, %s, %s)" % (__title__, message, duration, image))
     
     def debug(self, message):
-        '''
+        """
         logs a debug message
-        '''
+        """
         if self.enableDebug:
-            xbmc.log(__title__ + '::' + message)
+            xbmc.log(__title__ + "::" + message)
     
     def disable_autoexec(self):
-        '''
+        """
         Removes the autoexec line in special://home/userdata/autoexec.py
         Since version 2.0 this addon is run as a service. This line was needed in prior versions of the addon to allow for automatically starting the addon. 
         If this line is not removed after updating to version 2.0, the script would be started twice. 
         In short, this function allows for backward compatibility for updaters.
-        '''
+        """
         try:
             # See if the autoexec.py file exists
             if (os.path.exists(AUTOEXEC_PATH)):
                 found = False
-                autoexecfile = file(AUTOEXEC_PATH, 'r')
+                autoexecfile = file(AUTOEXEC_PATH, "r")
                 filecontents = autoexecfile.readlines()
                 autoexecfile.close()
                 
@@ -441,20 +432,20 @@ class Main:
                 for line in filecontents:
                     if line.find(__addonID__) > 0:
                        found = True
-                       __settings__.setSetting(id='remove_from_autoexec', value='true')
+                       __settings__.setSetting(id="remove_from_autoexec", value="true")
                 
                 # Found that we're in it and it's time to remove ourselves
                 if (found):
-                    autoexecfile = file(AUTOEXEC_PATH, 'w')
+                    autoexecfile = file(AUTOEXEC_PATH, "w")
                     for line in filecontents:
                         if not line.find(__addonID__) > 0:
                             autoexecfile.write(line)
                     autoexecfile.close()
-                    __settings__.setSetting(id='remove_from_autoexec', value='false')
+                    __settings__.setSetting(id="remove_from_autoexec", value="false")
                     self.debug("The autostart script was successfully removed from %s" % AUTOEXEC_PATH)
                 else:
                     self.debug("No need to remove the autostart script, as it was already removed from %s" % AUTOEXEC_PATH)
         except OSError, e:
-            self.debug('Removing the autostart script in %s failed with error code %d' % (AUTOEXEC_PATH, e.errno))
+            self.debug("Removing the autostart script in %s failed with error code %d" % (AUTOEXEC_PATH, e.errno))
 
 run = Main()
