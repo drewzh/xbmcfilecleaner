5430aa05f52943b769baf1ebf9a34f72e384a74c
Anthirian <gasmelt@gmail.com>
2013-03-09 16:46:40 +0100

    Added user settings for deleting folders and ignoring filetypes

--- default.py
+++ default.py
@@ -90,9 +90,11 @@ class Main:
                             if self.holding_enabled:
                                 if self.move_file(abs_path, self.holding_folder):
                                     count += 1
+                                    self.delete_empty_folders(os.path.dirname(abs_path))
                             else:
                                 if self.delete_file(abs_path):
                                     count += 1
+                                    self.delete_empty_folders(os.path.dirname(abs_path))
                         else:
                             self.debug("XBMC could not find the file at %s" % abs_path)
                     if count > 0:
@@ -112,9 +114,11 @@ class Main:
                                     new_path = self.holding_folder
                                 if self.move_file(abs_path, new_path):
                                     count += 1
+                                    self.delete_empty_folders(os.path.dirname(abs_path))
                             else:
                                 if self.delete_file(abs_path):
                                     count += 1
+                                    self.delete_empty_folders(os.path.dirname(abs_path))
                         else:
                             self.debug("XBMC could not find the file at %s" % abs_path)
                     if count > 0:
@@ -131,9 +135,11 @@ class Main:
                             if self.holding_enabled:
                                 if self.move_file(abs_path, self.holding_folder):
                                     count += 1
+                                    self.delete_empty_folders(os.path.dirname(abs_path))
                             else:
                                 if self.delete_file(abs_path):
                                     count += 1
+                                    self.delete_empty_folders(os.path.dirname(abs_path))
                         else:
                             self.debug("XBMC could not find the file at %s" % abs_path)
                     if count > 0:
@@ -230,6 +236,8 @@ class Main:
         __settings__ = xbmcaddon.Addon(__addonID__)
 
         self.deleting_enabled = bool(__settings__.getSetting("deleting_enabled") == "true")
+        self.delete_folders = bool(__settings__.getSetting("delete_folders") == "true")
+        self.ignore_extensions = str(__settings__.getSetting("ignore_extensions"))
         self.delayed_start = float(__settings__.getSetting("delayed_start"))
         self.scan_interval = float(__settings__.getSetting("scan_interval"))
 
@@ -344,17 +352,16 @@ class Main:
             self.debug("XBMC could not find the file at %s" % location)
             return False
 
-    def delete_empty_folders(self, file_path):
-        # TODO: Populate this list by loading it from the addon settings
-        ignored_file_types = [".nfo", ".srt", ".tbn", ".jpg", ".png"]
+    def delete_empty_folders(self, folder):
+        self.debug("Checking if %s is empty" % folder)
 
-        # TODO: Check if this works on any platform
-        #if not os.path.isdir(file_path):
-        #    file_path = os.path.dirname(file_path)
+        ignored_file_types = [file_ext.strip() for file_ext in self.ignore_extensions.split(",")]
 
-        folders, files = xbmcvfs.listdir(file_path)
+        self.debug("Ignoring file types %s" % ignored_file_types)
 
-        self.debug("Subfolders:\t" + str(folders) + "\nFiles:\t" + str(files))
+        subfolders, files = xbmcvfs.listdir(folder)
+
+        self.debug("Contents of %s:\nSubfolders:\t%s\nFiles:\t%s" % (folder, subfolders, files))
 
         empty = True
         try:
@@ -362,29 +369,30 @@ class Main:
                 _, ext = os.path.splitext(f)
                 self.debug("File extension: " + ext)
                 if ext not in ignored_file_types:
+                    self.debug("Found video file %s" % f)
                     empty = False
-        except Exception, e:
+        except OSError, oe:
+            self.debug("Error deriving file extension. Errno " + str(oe.errno))
             empty = False
 
         # Only delete directories if we found them to be empty (containing no files or filetypes we ignored)
         if empty:
             self.debug("Directory is empty and will be removed")
-
             try:
                 # Recursively delete any subfolders
-                for f in folders:
-                    self.debug("Deleting file at " + str(os.path.join(file_path, f)))
-                    self.delete_empty_folders(os.path.join(file_path, f))
+                for f in subfolders:
+                    self.debug("Deleting file at " + str(os.path.join(folder, f)))
+                    self.delete_empty_folders(os.path.join(folder, f))
 
                 # Delete any files in the current folder
                 for f in files:
-                    self.debug("Deleting file at " + str(os.path.join(file_path, f)))
-                    xbmcvfs.delete(os.path.join(file_path, f))
+                    self.debug("Deleting file at " + str(os.path.join(folder, f)))
+                    xbmcvfs.delete(os.path.join(folder, f))
 
                 # Finally delete the current folder
-                xbmcvfs.rmdir(file_path)
-            except Exception, e:
-                self.debug("An exception occurred while deleting folders: " + str(e))
+                xbmcvfs.rmdir(folder)
+            except OSError, oe:
+                self.debug("An exception occurred while deleting folders. Errno " + str(oe.errno))
         else:
             self.debug("Directory is not empty and will not be removed")
 
