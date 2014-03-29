64c751bc3e4417dd938300b22a027c43e149ad84
Anthirian <gasmelt@gmail.com>
2014-03-29 20:16:04 +0100

    Reuse existing window when clearing or trimming

--- viewer.py
+++ viewer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
-
 import xbmc
 import xbmcgui
 from xbmcaddon import Addon
-from utils import Log, notify
+from utils import Log, debug
 
 
 # Addon info
@@ -19,6 +17,8 @@ __profile__ = xbmc.translatePath(__addon__.getAddonInfo("profile")).decode("utf-
 class LogViewerDialog(xbmcgui.WindowXMLDialog):
     CAPTIONID = 101
     TEXTBOXID = 201
+    TRIMBUTTONID = 301
+    CLEARBUTTONID = 302
 
     def __init__(self, *args, **kwargs):
         self.log = Log()
@@ -27,27 +27,18 @@ class LogViewerDialog(xbmcgui.WindowXMLDialog):
 
     def onInit(self):
         self.getControl(self.CAPTIONID).setLabel(self.caption)
-        if len(sys.argv) > 1:
-            if sys.argv[1] == "show":
-                self.getControl(self.TEXTBOXID).setText(self.log.get())
-            elif sys.argv[1] == "trim":
-                # TODO: File is not trimmed yet
-                self.getControl(self.TEXTBOXID).setText(self.log.trim())
-            elif sys.argv[1] == "clear":
-                self.getControl(self.TEXTBOXID).setText(self.log.clear())
-            else:
-                self.getControl(self.TEXTBOXID).setText("Unknown argument %r" % sys.argv[1])
+        self.getControl(self.TEXTBOXID).setText(self.log.get())
+
+    def onClick(self, controlID, *args):
+        if controlID == self.TRIMBUTTONID:
+            self.getControl(self.TEXTBOXID).setText(self.log.trim())
+        elif controlID == self.CLEARBUTTONID:
+            self.getControl(self.TEXTBOXID).setText(self.log.clear())
         else:
-            self.getControl(self.TEXTBOXID).setText("Too few arguments")
-
-
-def show_logviewer():
-    path = __addon__.getAddonInfo("path")
-    win = LogViewerDialog("DialogLogViewer.xml", path, "Default")
-    win.doModal()
-    del win
+            debug("Unknown button pressed", xbmc.LOGERROR)
 
 
 if __name__ == "__main__":
-    # TODO: Reuse window when buttons are pressed
-    show_logviewer()
+    win = LogViewerDialog("DialogLogViewer.xml", __addon__.getAddonInfo("path"), "Default")
+    win.doModal()
+    del win
